f383802710931201ecdecada5110c5a1d7be41af
erarys <erarysabylkasym3@gmail.com>
2025-12-08 12:29:40 +0500

    add new widget

--- src/plugin-slots/WidgetSidebarSlot/index.jsx
+++ src/plugin-slots/WidgetSidebarSlot/index.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+// src/components/WidgetSidebarSlot.jsx
+import React from "react";
+import { PluginSlot } from "@openedx/frontend-plugin-framework";
+import LookingForChallengeWidget from "plugins/LookingForChallengeWidget";
 
-import { PluginSlot } from '@openedx/frontend-plugin-framework';
-import LookingForChallengeWidget from 'plugins/LookingForChallengeWidget';
-
-// eslint-disable-next-line arrow-body-style
 export const WidgetSidebarSlot = () => (
-  <PluginSlot
-    id="org.openedx.frontend.learner_dashboard.widget_sidebar.v1"
-    idAliases={['widget_sidebar_slot']}
-  >
+  <PluginSlot id="org.openedx.frontend.learner_dashboard.widget_sidebar.v1">
     <LookingForChallengeWidget />
+    {/* Новый виджет со ссылкой */}
+    <div className="custom-sidebar-widget">
+      <a
+        href="https://t.me/kaznuopen"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Легером группы
+      </a>
+    </div>
   </PluginSlot>
 );
 
